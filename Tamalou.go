9d4fc977e481f2f1e38021e78100d445dc1b9a82
Louis <artonge.c@gmail.com>
2017-05-11 17:06:21 +0200

    Remove MongoDB Example from main file

--- Tamalou.go
+++ Tamalou.go
@@ -2,10 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
-
-	mgo "gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
 
 	"github.com/blevesearch/bleve"
 )
@@ -42,49 +38,4 @@ func bleveExample() {
 		return
 	}
 	fmt.Println(searchResults)
-}
-
-// mgo example
-type person struct {
-	Name  string
-	Phone string
-}
-
-func mgoExemple() {
-
-	session, err := mgo.Dial("server1.example.com,server2.example.com")
-
-	if err != nil {
-		panic(err)
-	}
-
-	defer session.Close()
-
-	// Optional. Switch the session to a monotonic behavior.
-	session.SetMode(mgo.Monotonic, true)
-
-	c := session.DB("test").C("people")
-
-	err = c.Insert(
-		&person{"Ale", "+55 53 8116 9639"},
-		&person{"Cla", "+55 53 8402 8510"},
-	)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	result := person{}
-	err = c.Find(bson.M{"name": "Ale"}).One(&result)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Phone:", result.Phone)
-}
-
-type testDocument struct {
-	Title string
-	Note  string
 }

c77d4a3fe73b286122909e005a5b79c1b180f116
Wordam <niamor123@gmail.com>
2017-05-22 13:36:15 +0200

    modified get side effect func

--- Sider/Sider_test.go
+++ Sider/Sider_test.go
@@ -3,11 +3,17 @@ package sider
 import (
 	"fmt"
 	"testing"
-
-	"github.com/artonge/Tamalou/Queries"
 )
 
 var rawQuery = "Abdominal pain OR Gastrointestinal pain AND anorexia" //  AND Alopecia OR Decreased appetite OR Anxiety
+
+func TestSideEffect(t *testing.T) {
+	var drugId = "CID125880656"
+	sideffects, _ := GetSideEffects(drugId)
+	for _, se := range sideffects {
+		fmt.Println("Side effect ", se.SideEffectName)
+	}
+}
 
 // func BenchmarkSplitJoinQueries(b *testing.B) {
 // 	fmt.Println("Benchmarking sider split queries")
@@ -29,32 +35,32 @@ var rawQuery = "Abdominal pain OR Gastrointestinal pain AND anorexia" //  AND Al
 // 	}
 // }
 
-func TestQueryMeddraTree(t *testing.T) {
-
-	tree := Queries.ParseQuery(rawQuery)
-	//clinicalSigns := Queries.GetClinicalSigns(rawQuery)
-	results, err := QueryMeddraTree(tree)
-
-	fmt.Println("Got ", len(results), " results")
-
-	// Get frequency from all ids
-	//results, err = QueryMeddraFreq(results, clinicalSigns)
-
-	if err != nil {
-		fmt.Println("Error in Sider Test : \n	==>", err, "\n	==>", results)
-		t.Fail()
-	}
-
-	// for _, value := range results {
-	// 	fmt.Println("Stitch compound id =>>", value.StitchCompoundId)
-	// for _, se := range value.SideEffects {
-	// 	fmt.Println("\tSide Effect :")
-	// 	fmt.Println("\t\tName =>>", se.SideEffectName)
-	// 	fmt.Println("\t\tPlacebo =>>", se.Placebo)
-	// 	fmt.Println("\t\tFrequency =>>", se.Frequency)
-	// 	fmt.Println("\t\tFrequency Lower =>>", se.FrequencyLowerBound)
-	// 	fmt.Println("\t\tFrequency Upper=>>", se.FrequencyUpperBound)
-	// 	fmt.Println("\t\tMatched =>>", se.Matched)
-	// }
-	// }
-}
+// func TestQueryMeddraTree(t *testing.T) {
+//
+// 	tree := Queries.ParseQuery(rawQuery)
+// 	//clinicalSigns := Queries.GetClinicalSigns(rawQuery)
+// 	results, err := QueryMeddraTree(tree)
+//
+// 	fmt.Println("Got ", len(results), " results")
+//
+// 	// Get frequency from all ids
+// 	//results, err = QueryMeddraFreq(results, clinicalSigns)
+//
+// 	if err != nil {
+// 		fmt.Println("Error in Sider Test : \n	==>", err, "\n	==>", results)
+// 		t.Fail()
+// 	}
+//
+// 	// for _, value := range results {
+// 	// 	fmt.Println("Stitch compound id =>>", value.StitchCompoundId)
+// 	// for _, se := range value.SideEffects {
+// 	// 	fmt.Println("\tSide Effect :")
+// 	// 	fmt.Println("\t\tName =>>", se.SideEffectName)
+// 	// 	fmt.Println("\t\tPlacebo =>>", se.Placebo)
+// 	// 	fmt.Println("\t\tFrequency =>>", se.Frequency)
+// 	// 	fmt.Println("\t\tFrequency Lower =>>", se.FrequencyLowerBound)
+// 	// 	fmt.Println("\t\tFrequency Upper=>>", se.FrequencyUpperBound)
+// 	// 	fmt.Println("\t\tMatched =>>", se.Matched)
+// 	// }
+// 	// }
+// }
